1e99f69361d7f7840a9966e89dec4e8c18a31c1d
vikasreddy85 <vikasreddy2313@gmail.com>
2026-01-25 19:23:19 -0500

    fix: updated backend package.json and docker entryfile

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 set -e
 
-
 PRISMA_DONE_FILE="/app/.prisma_done"
 
 if [ ! -f "$PRISMA_DONE_FILE" ]; then
   echo "Running Prisma generate and DB push..."
-  pnpm prisma generate
-  pnpm prisma db push
-  pnpm migrate-data
+  pnpm exec prisma generate
+  pnpm exec prisma db push
+  pnpm run migrate-data
   touch "$PRISMA_DONE_FILE"
 fi
 
